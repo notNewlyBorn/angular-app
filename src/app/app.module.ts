d0b3e4d33d6f38ec9add10f56afccae882482af9
notnewlyborn <nafis_20@yahoo.com>
2017-06-27 15:48:04 +0200

    changes pushed to remote

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
@@ -17,7 +17,7 @@ import { SortPipe } from './app.sortPipe';
   ],
   imports: [
     BrowserModule,
-    FormsModule,
+    ReactiveFormsModule,
     RouterModule.forRoot([
       {
         path: 'members',
